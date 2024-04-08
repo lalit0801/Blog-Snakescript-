509363185e3eab998ab61f0be337aac8115f66a8
Lalit <lalitdream999@gmail.com>
2024-04-08 21:40:16 +0530

    url conf, views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns=[
-    
-]
+urlpatterns = [
+   
+    path('create/', views.CreateBlogView.as_view(), name='Create-Blog'),
+    path('<str:id>/', views.BlogDetailView.as_view(), name='Single-Blog-Details'),
+    path('list/',views.BlogListView.as_view(), name='ListAll-Blogs'),
+    path('list/author/', views.CurrentUserBlogListView.as_view(), name='Current_user_BlogListView'),
+    path('update/<str:id>/', views.UpdateBlogView.as_view(), name='Update-Blog'),
+    path('delete/<str:id>/', views.DeleteBlogView.as_view(), name='Delete-Blog'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from .models import Blog
+from rest_framework import permissions
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.parsers import MultiPartParser
 
-# Create your views here.
+
+class CreateBlogView( generics.CreateAPIView):
+    queryset= Blog.objects.all()
+    serializer_class= serializers.CreateBlogSerializer
+    permission_classes= [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    @swagger_auto_schema(request_body=serializers.UpdateBlogSerializer)
+    def post(self, request):
+        
+        request.data['author']= request.user.id
+
+        return super().post(request)
+
+
+class  BlogDetailView(generics.RetrieveAPIView):
+    queryset= Blog.objects.all() 
+    serializer_class= serializers.BlogDetailSerializer
+    lookup_field= 'id'
+
+class  BlogListView(generics.ListAPIView):
+    serializer_class= serializers.BlogDetailSerializer
+    def get_queryset(self):
+
+        return Blog.objects.all().order_by('-updated_at')
+
+
+class  CurrentUserBlogListView(generics.ListAPIView): 
+    serializer_class= serializers.BlogDetailSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+
+        return Blog.objects.filter(user=self.request.user.id).order_by('-updated_at')
+
+class UpdateBlogView( generics.UpdateAPIView):
+    serializer_class= serializers.UpdateBlogSerializer
+    permission_classes=[permissions.IsAuthenticated]
+    lookup_field= 'id'
+    def get_queryset(self):
+        return Blog.objects.filter(user=self.request.user.id)
+
+class DeleteBlogView( generics.DestroyAPIView):
+    serializer_class= serializers.UpdateBlogSerializer
+    permission_classes=[permissions.IsAuthenticated]
+    lookup_field= 'id'
+    def get_queryset(self):
+        return Blog.objects.filter(user=self.request.user.id)
